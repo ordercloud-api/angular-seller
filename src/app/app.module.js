e01bbbed659dc0c0f3ebe3e88239e92df3c1831c
Alexa Rarick <ararick@four51.com>
2017-01-04 10:46:54 -0600

    CCN-250 Install angular-ui-select

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -6,6 +6,7 @@ angular.module('orderCloud', [
         'ui.tree',
         'ui.router',
         'ui.bootstrap',
+        'ui-select',
         'orderCloud.sdk',
         'LocalForageModule',
         'toastr',
