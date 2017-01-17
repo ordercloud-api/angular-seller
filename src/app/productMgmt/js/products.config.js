9483586fd5e8dd8018450bbd6f5c8314587bf74a
Kyle Olson <kolson@four51.com>
2017-01-16 19:44:48 -0600

    CCN-183 Product Management major refactor

---
+++ src/app/productMgmt/js/products.config.js
@@ -0,0 +1,73 @@
+angular.module('orderCloud')
+    .config(ProductsConfig)
+;
+
+function ProductsConfig($stateProvider) {
+    $stateProvider
+        .state('products', {
+            parent: 'base',
+            templateUrl: 'productMgmt/templates/products.html',
+            controller: 'ProductsCtrl',
+            controllerAs: 'products',
+            url: '/products?from&to&search&page&pageSize&searchOn&sortBy&filters',
+            data: {componentName: 'Products'},
+            resolve: {
+                Parameters: function($stateParams, OrderCloudParameters) {
+                    return OrderCloudParameters.Get($stateParams);
+                },
+                ProductList: function(OrderCloud, Parameters) {
+                    return OrderCloud.Products.List(Parameters.search, Parameters.page, Parameters.pageSize || 12, Parameters.searchOn, Parameters.sortBy, Parameters.filters);
+                }
+            }
+        })
+        .state('products.create', {
+            url: '/create',
+            templateUrl: 'productMgmt/templates/productCreate.html',
+            controller: 'ProductCreateCtrl',
+            controllerAs: 'productCreate'
+        })
+        .state('products.detail', {
+            url: '/:productid/detail?page',
+            templateUrl: 'productMgmt/templates/productDetail.html',
+            controller: 'ProductDetailCtrl',
+            controllerAs: 'productDetail',
+            resolve: {
+                Parameters: function($stateParams, OrderCloudParameters) {
+                    return OrderCloudParameters.Get($stateParams);
+                },
+                SelectedProduct: function ($stateParams, OrderCloud) {
+                    return OrderCloud.Products.Get($stateParams.productid);
+                },
+                AssignmentList: function(ocProductsService, Parameters, buyerid) {
+                    return ocProductsService.AssignmentList(Parameters, buyerid);
+                },
+                //when we group together the price schedules by the id , it messes with the pagination, I would would have to update the meta data before it resolves , and then translate the results.
+                AssignmentData: function (ocProductsService, AssignmentList) {
+                    return ocProductsService.AssignmentData(AssignmentList);
+                }
+            }
+        })
+        .state('products.detail.createAssignment', {
+            url: '/assign',
+            templateUrl: 'productMgmt/templates/productCreateAssignment.html',
+            controller: 'ProductCreateAssignmentCtrl',
+            controllerAs: 'productCreateAssignment',
+            resolve: {
+                Buyers: function(OrderCloud){
+                    return OrderCloud.Buyers.List();
+                }
+            }
+        })
+        .state('products.detail.priceScheduleDetail', {
+            url: '/:pricescheduleid',
+            templateUrl: 'productMgmt/templates/priceScheduleDetail.html',
+            controller: 'PriceScheduleDetailCtrl',
+            controllerAs: 'priceScheduleDetail',
+            resolve: {
+                AssignmentDataDetail: function($stateParams, ocProductsService, AssignmentData) {
+                    return ocProductsService.AssignmentDataDetail(AssignmentData, $stateParams.pricescheduleid);
+                }
+            }
+        })
+    ;
+}
---
+++ src/app/productMgmt/js/products.service.js
@@ -0,0 +1,228 @@
+angular.module('orderCloud')
+    .factory('ocProductsService', ocProductsService)
+;
+
+function ocProductsService($q, OrderCloud, PriceBreak) {
+    var service = {
+        AssignmentList: _assignmentList,
+        AssignmentData: _assignmentData,
+        AssignmentDataDetail: _assignmentDataDetail,
+        AssignBuyer: _assignBuyer,
+        CreateNewPriceScheduleAndAssignments: _createNewPriceScheduleAndAssignments
+    };
+
+    function _assignmentList(parameters, buyerid) {
+        var deferred = $q.defer();
+
+        OrderCloud.BuyerID.Set(undefined);
+
+        var page = 1;
+        var pageSize = 100;
+        OrderCloud.Products.ListAssignments(parameters.productid, null, null, null, null, page, pageSize, null)
+            .then(function(data) {
+                var queue = [];
+                var assignments = data;
+                if (data.Meta.TotalPages > data.Meta.Page) {
+                    page = data.Meta.Page;
+                    while (page < data.Meta.TotalPages) {
+                        page += 1;
+                        queue.push(OrderCloud.Products.ListAssignments(parameters.productid, null, null, null, null, page, data.Meta.PageSize, null));
+                    }
+                    return $q.all(queue)
+                        .then(function(results) {
+                            angular.forEach(results, function(result) {
+                                assignments.Items = [].concat(assignments.Items, result.Items);
+                                assignments.Meta = result.Meta;
+                            });
+                            OrderCloud.BuyerID.Set(buyerid);
+                            assignments.buyerlist = _.uniq(_.pluck(assignments.Items, 'BuyerID'));
+
+                            deferred.resolve(assignments);
+                        });
+                } else{
+                    OrderCloud.BuyerID.Set(buyerid);
+                    assignments.buyerlist = _.uniq(_.pluck(assignments.Items, 'BuyerID'));
+                    deferred.resolve(assignments);
+                }
+            });
+
+        return deferred.promise;
+    }
+
+    function _assignmentData(assignments) {
+        var deferred = $q.defer();
+
+        var psQueue = [];
+        var schedules = _.uniq(_.pluck(assignments.Items, 'PriceScheduleID'));
+
+        angular.forEach(schedules, function(id) {
+            psQueue.push(OrderCloud.PriceSchedules.Get(id));
+        });
+        $q.all(psQueue)
+            .then(function(results) {
+                angular.forEach(results, function(ps) {
+                    angular.forEach(_.where(assignments.Items, {PriceScheduleID: ps.ID}), function(p) {
+                        p.PriceSchedule = ps;
+                    });
+                });
+                groupBy();
+            });
+
+        function groupBy() {
+            var results = {};
+            var priceSchedules = _.groupBy(assignments.Items, 'PriceScheduleID');
+            angular.forEach(priceSchedules, function(assignments, key) {
+                results[key] = {
+                    PriceSchedule: assignments[0].PriceSchedule,
+                    Buyers: [],
+                    UserGroups: []
+                };
+                angular.forEach(assignments,function(details) {
+                    if (details.BuyerID && !details.UserGroupID) {
+                        results[key].Buyers.push(details.BuyerID);
+                    }
+                    else if (details.BuyerID && details.UserGroupID) {
+                        results[key].UserGroups.push({BuyerID: details.BuyerID, UserGroupID: details.UserGroupID});
+                    }
+                });
+            });
+
+            deferred.resolve(results);
+        }
+
+        return deferred.promise;
+    }
+
+    function _assignmentDataDetail(assignmentData, priceScheduleID) {
+        var deferred = $q.defer();
+        var data = assignmentData[priceScheduleID];
+        var result = {
+            PriceSchedule: data.PriceSchedule,
+            Buyers: []
+        };
+
+        var buyerChunks = chunks(_.uniq(data.Buyers.concat(_.uniq(_.pluck(data.UserGroups, 'BuyerID')))));
+        var userGroupGroups = _.groupBy(data.UserGroups, 'BuyerID');
+        var userGroupChunks = [];
+        angular.forEach(userGroupGroups, function(group) {
+            userGroupChunks = userGroupChunks.concat(chunks(group));
+        });
+
+        var buyerQueue = [];
+        var userGroupQueue = [];
+
+        angular.forEach(buyerChunks, function(chunk) {
+            buyerQueue.push(OrderCloud.Buyers.List(null, null, null, null, null, {ID: chunk.join('|')}));
+        });
+
+        angular.forEach(userGroupChunks, function(chunk) {
+            userGroupQueue.push((function() {
+                var d = $q.defer();
+                var buyerID = chunk[0].BuyerID;
+
+                OrderCloud.UserGroups.List(null, null, null, null, null, {ID: _.pluck(chunk, 'UserGroupID').join('|')}, buyerID)
+                    .then(function(data) {
+                        angular.forEach(_.where(result.Buyers, {ID: buyerID}), function(buyer) {
+                            if (!buyer.UserGroups) buyer.UserGroups = [];
+                            buyer.UserGroups = buyer.UserGroups.concat(data.Items);
+                        });
+                        d.resolve();
+                    });
+
+                return d.promise;
+            })());
+        });
+
+        $q.all(buyerQueue)
+            .then(function(results) {
+                angular.forEach(results, function(r) {
+                    _.map(r.Items, function(buyer) {
+                        buyer.Assigned = data.Buyers.indexOf(buyer.ID) > -1;
+                    });
+                    result.Buyers = result.Buyers.concat(r.Items);
+                });
+                getUserGroups();
+            });
+
+        function getUserGroups() {
+            $q.all(userGroupQueue)
+                .then(function(results) {
+                    deferred.resolve(result);
+                });
+        }
+
+        function chunks(list) {
+            var i, j, listChunks = [], chunkSize = 10;
+            for (i = 0, j = list.length; i < j; i += chunkSize) {
+                listChunks.push(list.slice(i, i + chunkSize));
+            }
+            return listChunks;
+        }
+
+        return deferred.promise;
+    }
+
+    //This method not only assigns the buyer company to the price schedule, but it removes any user group assignments under that buyer as well
+    function _assignBuyer(buyer, productid, pricescheduleid) {
+        var deferred = $q.defer();
+
+        var queue = [];
+        angular.forEach(buyer.UserGroups, function(group) {
+            queue.push(OrderCloud.Products.DeleteAssignment(productid, null, group.ID, buyer.ID));
+        });
+        queue.push(OrderCloud.Products.SaveAssignment({BuyerID: buyer.ID, ProductID: productid, PriceScheduleID: pricescheduleid}));
+
+        $q.all(queue).then(function() {
+            buyer.UserGroups = [];
+            deferred.resolve(buyer);
+        });
+
+        return deferred.promise;
+    }
+
+    function _createNewPriceScheduleAndAssignments(product, priceSchedule, selectedBuyer, selectedUserGroups) {
+        var deferred = $q.defer();
+
+        priceSchedule = PriceBreak.SetMinMax(priceSchedule);
+
+        OrderCloud.PriceSchedules.Create(priceSchedule)
+            .then(function(ps) {
+                var assignment = {
+                    ProductID: product.ID,
+                    PriceScheduleID: ps.ID,
+                    BuyerID: selectedBuyer.ID
+                };
+                if (selectedBuyer && (selectedUserGroups == null || selectedUserGroups.length == 0 )) {
+                    OrderCloud.Products.SaveAssignment(assignment)
+                        .then(function(data){
+                            deferred.resolve(data);
+                        })
+                        .catch(function (error) {
+                            deferred.reject(error);
+                        });
+                }
+                else if (vselectedBuyer && vselectedUserGroups.length > 0 ) {
+                    var assignmentQueue = [];
+                    angular.forEach(vm.selectedUserGroups, function(usergroup) {
+                        var userGroupAssignment = angular.copy(assignment);
+                        userGroupAssignment.UserGroupID = usergroup.ID;
+                        assignmentQueue.push(OrderCloud.Products.SaveAssignment(userGroupAssignment));
+                    });
+                    $q.all(assignmentQueue)
+                        .then(function (data) {
+                            deferred.resolve(data);
+                        })
+                        .catch(function (error) {
+                            deferred.reject(error);
+                        });
+                }
+            })
+            .catch(function (ex) {
+                deferred.reject(ex);
+            });
+
+        return deferred.promise;
+    }
+
+    return service;
+}
