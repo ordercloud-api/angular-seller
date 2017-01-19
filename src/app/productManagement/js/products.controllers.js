4c7c462faa550a594e30851a46634054f90c3ef0
Kyle Olson <kolson@four51.com>
2017-01-19 08:19:10 -0600

    CCN-183 Product Inventory updates

--- src/app/productManagement/js/products.controllers.js
+++ src/app/productManagement/js/products.controllers.js
@@ -452,12 +452,14 @@ function ProductShippingController(toastr, OrderCloud, AdminAddresses) {
 function ProductInventoryController(toastr, ocProductsService, ProductInventory) {
     var vm = this;
     vm.inventory = angular.copy(ProductInventory);
+    vm.inventoryAvailable = angular.copy(ProductInventory.Available);
     vm.updateProductInventory = updateProductInventory;
 
     function updateProductInventory(product) {
         vm.productUpdateLoading = ocProductsService.UpdateInventory(product, vm.inventory)
             .then(function(inventory) {
                 vm.inventory = angular.copy(inventory);
+                vm.inventoryAvailable = angular.copy(inventory.Available);
                 vm.ProductInventoryForm.$setPristine();
                 toastr.success(product.Name + ' inventory was updated', 'Success!');
             });
