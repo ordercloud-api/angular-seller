f0abc374118fbc73aad81cac675b01f3c1883e88
Robert Watt <rwatt@four51.com>
2017-02-15 11:35:49 -0600

    "deploy" task added
    Running `gulp deploy` will now compile and deploy the result
    to a gh-pages branch on the default remote for your repository.
    This gh-pages branch can then be merged with the base fork
    to update the ordercloud-api/angular-admin gh-pages

---
+++ gulp/compile/deploy.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    ghPages = require('gulp-gh-pages'),
+    htmlReplace = require('gulp-html-replace'),
+    repoName = require('git-repo-name'),
+    config = require('../../gulp.config');
+
+gulp.task('deploy', ['href'], function() {
+    return gulp.src(config.compile + "**/*")
+        .pipe(ghPages({
+            force: true,
+            message: "Deploy to gh-pages"
+        }))
+});
+
+gulp.task('href', ['index'], function() {
+    return gulp.src(config.compile + 'index.html')
+        .pipe(htmlReplace({
+            'href': {
+                src: repoName.sync(),
+                tpl: '<base href="/%s/" />'
+            }
+        }))
+        .pipe(gulp.dest(config.compile))
+});
