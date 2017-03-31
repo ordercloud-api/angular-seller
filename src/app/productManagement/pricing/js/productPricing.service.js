ae5d22a80763a219540bdeb82251177c3005efc2
Kyle Olson <kolson@four51.com>
2017-03-31 15:20:34 -0500

    Price Schedule Create fix

--- src/app/productManagement/pricing/js/productPricing.service.js
+++ src/app/productManagement/pricing/js/productPricing.service.js
@@ -2,7 +2,7 @@ angular.module('orderCloud')
     .factory('ocProductPricing', ocProductPricingService)
 ;
 
-function ocProductPricingService($q, $uibModal, OrderCloud, ocConfirm) {
+function ocProductPricingService($q, $uibModal, OrderCloud, sdkOrderCloud, ocConfirm) {
     var service = {
         AssignmentList: _assignmentList,
         AssignmentData: _assignmentData,
@@ -233,30 +233,38 @@ function ocProductPricingService($q, $uibModal, OrderCloud, ocConfirm) {
                     PriceScheduleID: ps.ID,
                     BuyerID: selectedBuyer.ID
                 };
-                if (selectedBuyer && (selectedUserGroups == null || selectedUserGroups.length == 0 )) {
-                    OrderCloud.Products.SaveAssignment(assignment)
-                        .then(function(data){
-                            deferred.resolve(data);
-                        })
-                        .catch(function (error) {
-                            deferred.reject(error);
-                        });
-                }
-                else if (selectedBuyer && selectedUserGroups.length > 0 ) {
-                    var assignmentQueue = [];
-                    angular.forEach(selectedUserGroups, function(usergroup) {
-                        var userGroupAssignment = angular.copy(assignment);
-                        userGroupAssignment.UserGroupID = usergroup.ID;
-                        assignmentQueue.push(OrderCloud.Products.SaveAssignment(userGroupAssignment));
+                var catalogAssignment = {
+                    catalogID: selectedBuyer.DefaultCatalogID,
+                    productID: product.ID
+                };
+                sdkOrderCloud.Catalogs.SaveProductAssignment(catalogAssignment)
+                    .then(function() {
+                        if (selectedBuyer && (selectedUserGroups == null || selectedUserGroups.length == 0 )) {
+                            OrderCloud.Products.SaveAssignment(assignment)
+                                .then(function(data){
+                                    deferred.resolve(data);
+                                })
+                                .catch(function (error) {
+                                    deferred.reject(error);
+                                });
+                        }
+                        else if (selectedBuyer && selectedUserGroups.length > 0 ) {
+                            var assignmentQueue = [];
+                            angular.forEach(selectedUserGroups, function(usergroup) {
+                                var userGroupAssignment = angular.copy(assignment);
+                                userGroupAssignment.UserGroupID = usergroup.ID;
+                                assignmentQueue.push(OrderCloud.Products.SaveAssignment(userGroupAssignment));
+                            });
+                            $q.all(assignmentQueue)
+                                .then(function (data) {
+                                    deferred.resolve(data);
+                                })
+                                .catch(function (error) {
+                                    deferred.reject(error);
+                                });
+                        }
                     });
-                    $q.all(assignmentQueue)
-                        .then(function (data) {
-                            deferred.resolve(data);
-                        })
-                        .catch(function (error) {
-                            deferred.reject(error);
-                        });
-                }
+                
             })
             .catch(function (ex) {
                 deferred.reject(ex);
