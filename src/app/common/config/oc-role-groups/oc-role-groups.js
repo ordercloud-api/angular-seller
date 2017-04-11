4dd8aa604952fef5320503fee9aad34af83689fc
Robert Watt <rwatt@four51.com>
2017-04-11 13:04:55 -0500

    Update app.constants.json scope to be more readable
    - Included some missing roles (all administrative roles should be requested now, this can
    change for customized applications)
    - common/config/oc-role-groups has been updated to reflect the newly available roles

--- src/app/common/config/oc-role-groups/oc-role-groups.js
+++ src/app/common/config/oc-role-groups/oc-role-groups.js
@@ -1,20 +1,24 @@
 angular.module('orderCloud')
     .config(function(ocRolesProvider) {
         var roleGroups = [
-            {Name: 'BuyerRoles', Type: 'Any', Roles: ['BuyerReader', 'BuyerAdmin']},
-            {Name: 'ProductRoles', Type: 'Any', Roles: ['ProductReader', 'ProductAdmin']},
-            {Name: 'OrderRoles', Type: 'Any', Roles: ['OrderReader', 'OrderAdmin']},
-            {Name: 'SellerUserRoles', Type: 'Any', Roles: ['AdminUserReader', 'AdminUserAdmin']},
-            {Name: 'SellerUserGroupRoles', Type: 'Any', Roles: ['AdminUserGroupReader', 'AdminUserGroupAdmin']},
-            {Name: 'BuyerUserRoles', Type: 'Any', Roles: ['BuyerUserReader', 'BuyerUserAdmin']},
-            {Name: 'UserGroupRoles', Type: 'Any', Roles: ['UserGroupReader', 'UserGroupAdmin']},
-            {Name: 'CategoryRoles', Type: 'Any', Roles: ['CategoryReader', 'CategoryAdmin']},
-            {Name: 'AddressRoles', Type: 'Any', Roles: ['AddressReader', 'AddressAdmin']}, //For both seller & buyer address management
-            {Name: 'CreditCardRoles', Type: 'Any', Roles: ['CreditCardReader', 'CreditCardAdmin']},
-            {Name: 'SpendingAccountRoles', Type: 'Any', Roles: ['SpendingAccountReader', 'SpendingAccountAdmin']},
-            {Name: 'CostCenterRoles', Type: 'Any', Roles: ['CostCenterReader', 'CostCenterAdmin']},
-            {Name: 'ApprovalRuleRoles', Type: 'Any', Roles: ['ApprovalRuleReader', 'ApprovalRuleAdmin']},
-            {Name: 'PromotionRoles', Type: 'Any', Roles: ['PromotionReader', 'PromotionAdmin']}
+            {Name: 'AddressRoles',              Type: 'Any', Roles: ['AddressReader', 'AddressAdmin']}, //For both seller & buyer address management
+            {Name: 'ApprovalRuleRoles',         Type: 'Any', Roles: ['ApprovalRuleReader', 'ApprovalRuleAdmin']},
+            {Name: 'BuyerRoles',                Type: 'Any', Roles: ['BuyerReader', 'BuyerAdmin']},
+            {Name: 'BuyerUserRoles',            Type: 'Any', Roles: ['BuyerUserReader', 'BuyerUserAdmin']},
+            {Name: 'CategoryRoles',             Type: 'Any', Roles: ['CategoryReader', 'CategoryAdmin']},
+            {Name: 'CostCenterRoles',           Type: 'Any', Roles: ['CostCenterReader', 'CostCenterAdmin']},
+            {Name: 'CreditCardRoles',           Type: 'Any', Roles: ['CreditCardReader', 'CreditCardAdmin']},
+            {Name: 'OrderRoles',                Type: 'Any', Roles: ['OrderReader', 'OrderAdmin']},
+            {Name: 'PromotionRoles',            Type: 'Any', Roles: ['PromotionReader', 'PromotionAdmin']},
+            {Name: 'ProductRoles',              Type: 'Any', Roles: ['ProductReader', 'ProductAdmin']},
+            {Name: 'SellerUserGroupRoles',      Type: 'Any', Roles: ['AdminUserGroupReader', 'AdminUserGroupAdmin']},
+            {Name: 'SellerUserRoles',           Type: 'Any', Roles: ['AdminUserReader', 'AdminUserAdmin']},
+            {Name: 'ShipmentRoles',             Type: 'Any', Roles: ['ShipmentReader', 'ShipmentAdmin']},
+            {Name: 'SpendingAccountRoles',      Type: 'Any', Roles: ['SpendingAccountReader', 'SpendingAccountAdmin']},
+            {Name: 'SupplierRoles',             Type: 'Any', Roles: ['SupplierReader', 'SupplierAdmin']},
+            {Name: 'SupplierUserGroupRoles',    Type: 'Any', Roles: ['SupplierUserGroupReader', 'SupplierUserGroupAdmin']},
+            {Name: 'SupplierUserRoles',         Type: 'Any', Roles: ['SupplierUserReader', 'SupplierUserAdmin']},
+            {Name: 'UserGroupRoles',            Type: 'Any', Roles: ['UserGroupReader', 'UserGroupAdmin']}
         ];
 
         angular.forEach(roleGroups, function(roleGroup) {
