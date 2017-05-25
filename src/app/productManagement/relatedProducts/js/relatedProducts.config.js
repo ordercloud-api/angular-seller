5762727605b51681b3aa3080cce351cd9eddb655
Alexa Rarick <ararick@four51.com>
2017-05-25 15:52:51 -0500

    F51-27 feat: Get all products for related products view, add check box to toggle true/false on related

--- src/app/productManagement/relatedProducts/js/relatedProducts.config.js
+++ src/app/productManagement/relatedProducts/js/relatedProducts.config.js
@@ -8,24 +8,33 @@ function RelatedProductConfig($stateProvider) {
             url: '/related-products?page&pageSize',
             templateUrl: 'productManagement/relatedProducts/templates/relatedProducts.html',
             controller: 'RelatedProductCtrl',
-            controllerAs: 'relatedProduct',
+            controllerAs: 'relatedProducts',
             resolve: {
                 Parameters: function($stateParams, ocParameters) {
                     return ocParameters.Get($stateParams);
                 },
-                RelatedProductsList: function(OrderCloudSDK, SelectedProduct, Parameters) {
-                    if (SelectedProduct.xp && SelectedProduct.xp.RelatedProducts && SelectedProduct.xp.RelatedProducts.length) {
-                        var parameters = {
-                            filters: {
-                                ID: SelectedProduct.xp.RelatedProducts.join('|')
-                            },
-                            pageSize: 15,
-                            page: Parameters.page || 1
-                        };
-                        return OrderCloudSDK.Products.List(parameters);
-                    } else {
-                        return {Items: []}
-                    }
+                ProductsList: function(OrderCloudSDK, SelectedProduct, Parameters) {
+                    return OrderCloudSDK.Products.List(Parameters)
+                        .then(function(products) {
+                            if (SelectedProduct.xp && SelectedProduct.xp.RelatedProducts && SelectedProduct.xp.RelatedProducts.length) {
+                                var relatedProductIDs = SelectedProduct.xp.RelatedProducts;
+                                    _.each(relatedProductIDs, function(id) {
+                                        var related = _.pluck(products.Items, 'ID').indexOf(id) > -1;
+                                        if (related) {
+                                            var relatedProduct = _.findWhere(products.Items, {ID: id});
+                                            relatedProduct.Related = true;
+                                            return relatedProduct;
+                                        }
+                                    })
+                                    var index = products.Items.findIndex(function(product) {
+                                        return product.ID === SelectedProduct.ID;
+                                    })
+                                    products.Items.splice(index, 1);
+                                    return products;
+                            } else {
+                                return products;
+                            }
+                        })
                 }
             }
         })
--- src/app/productManagement/relatedProducts/js/relatedProducts.controller.js
+++ src/app/productManagement/relatedProducts/js/relatedProducts.controller.js
@@ -2,110 +2,52 @@ angular.module('orderCloud')
     .controller('RelatedProductCtrl', RelatedProductController)
 ;
 
-function RelatedProductController($q, OrderCloudSDK, toastr, $state, $exceptionHandler, RelatedProductsList, SelectedProduct, Parameters) {
+function RelatedProductController($q, OrderCloudSDK, toastr, $state, $exceptionHandler, ProductsList, SelectedProduct, Parameters) {
     var vm = this;
     vm.product = angular.copy(SelectedProduct);
-    vm.relatedProducts = RelatedProductsList;
+    vm.products = ProductsList;
     vm.parameters = Parameters;
     vm.selectedProducts = [];
     vm.uiSelectProducts;
 
-    vm.removeRelatedProduct = removeRelatedProduct;
-    vm.addRelatedProducts = addRelatedProducts;
-    vm.listAllProducts = listAllProducts;
+    vm.updateProduct = updateProduct;
     vm.pageChanged = pageChanged;
     vm.loadMore = loadMore;
 
-    function removeRelatedProduct(relatedProduct) {
-        var list = _.without(vm.product.xp.RelatedProducts, relatedProduct.ID);
-        OrderCloudSDK.Products.Patch(vm.product.ID, {xp: {RelatedProducts: list}})
+    function updateProduct(product) {
+        var relatedArr,
+            message;
+        if (product.Related) {
+            if (vm.product.xp && vm.product.xp.RelatedProducts && vm.product.xp.RelatedProducts.length) {
+                relatedArr = vm.product.xp.RelatedProducts;
+                relatedArr.push(product.ID);
+            } else {
+                related = product.ID;
+            }
+            message = product.Name + ' was added to related products';
+        } else {
+            relatedArr = _.without(vm.product.xp.RelatedProducts, product.ID);
+            message = product.Name + ' was removed from Related Products';
+        }
+        OrderCloudSDK.Products.Patch(vm.product.ID, {xp: {RelatedProducts: relatedArr}})
             .then(function() {
-                vm.product.xp.RelatedProducts = list;
-                toastr.success(relatedProduct.Name + ' was removed from Related Products', 'Success!')
+                toastr.success(message, 'Success')
             })
             .catch(function(error) {
                 $exceptionHandler(error);
             })
-            .finally(function(){
-                $state.reload();
-            });
-    }
-
-    function addRelatedProducts(products) {
-        var filteredList = _.filter(products, function(productList) {
-            return productList.ID !== vm.product.ID;
-        });
-        if(vm.product.xp && vm.product.xp.RelatedProducts && vm.product.xp.RelatedProducts.length) {
-            var existingProducts = vm.product.xp.RelatedProducts;
-            var productIDs = _.pluck(filteredList, 'ID');
-            var relatedProductIDs = _.union(existingProducts, productIDs);
-
-            OrderCloudSDK.Products.Patch(vm.product.ID, {xp: {RelatedProducts: relatedProductIDs}})
-                .then(function() {
-                    toastr.success('Product(s) added to Related Products', 'Success!')
-                })
-                .catch(function(error) {
-                    $exceptionHandler(error);
-                })
-                .finally(function(){
-                    $state.reload();
-                });
-        } else {
-            var productIDs = _.pluck(filteredList, 'ID');
-
-            OrderCloudSDK.Products.Patch(vm.product.ID, {xp: {RelatedProducts: productIDs}})
-                .then(function() {
-                    toastr.success('Product(s) added to Related Products', 'Success!')
-                })
-                .catch(function(error) {
-                    $exceptionHandler(error);
-                })
-                .finally(function(){
-                    $state.reload();
-                });
-        }
-    }
-
-    function listAllProducts(product) {
-        var page = 1;
-        var parameters = {
-            pageSize: 100,
-            page: page,
-            search: product
-        };
-        return OrderCloudSDK.Products.List(parameters)
-            .then(function(data){
-                vm.uiSelectProducts = data;
-                var queue = [];
-                if(data.Meta.TotalPages > data.Meta.Page) {
-                    page = data.Meta.Page;
-                    while (page < data.Meta.TotalPages) {
-                        page += 1;
-                        parameters.page = page;
-                        queue.push(OrderCloudSDK.Products.List(parameters));
-                    }
-                }
-                return $q.all(queue)
-                    .then(function(allProducts) {
-                        _.each(allProducts, function(productArr) {
-                            vm.uiSelectProducts.Items = [].concat(vm.uiSelectProducts.Items, productArr.Items);
-                            vm.uiSelectProducts.Meta = productArr.Meta;
-                        });
-                        return vm.uiSelectProducts;
-                    })
-            });
     }
 
     function pageChanged() {
-        $state.go('product.relatedProducts', {page: vm.relatedProducts.Meta.Page}, {reload: true});
+        $state.go('product.relatedProducts', {page: vm.products.Meta.Page}, {reload: true});
     }
 
     function loadMore() {
-        var parameters = angular.extend(Parameters, {page: vm.relatedProducts.Meta.Page + 1, filters: {ID: vm.product.xp.RelatedProducts.join('|')}});
+        var parameters = angular.extend(Parameters, {page: vm.products.Meta.Page + 1, filters: {ID: vm.product.xp.RelatedProducts.join('|')}});
         return OrderCloudSDK.Products.List(parameters)
             .then(function(data) {
-                vm.relatedProducts.Items = vm.relatedProducts.Items.concat(data.Items);
-                vm.relatedProducts.Meta = data.Meta;
+                vm.products.Items = vm.products.Items.concat(data.Items);
+                vm.products.Meta = data.Meta;
             });
     }
 }
