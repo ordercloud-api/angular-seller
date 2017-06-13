a60d13835e90bcea0c313e1494a4d798c44f2db4
Alexa Rarick <ararick@four51.com>
2017-06-13 07:48:04 -0500

    F51-244 feat: add toggle functionality for product zoom, update variables for new data model

--- src/app/productManagement/images/js/productImages.controller.js
+++ src/app/productManagement/images/js/productImages.controller.js
@@ -2,14 +2,17 @@ angular.module('orderCloud')
     .controller('ProductImagesCtrl', ProductImagesController)
 ;
 
-function ProductImagesController(OrderCloudSDK, SelectedProduct) {
+function ProductImagesController(OrderCloudSDK, SelectedProduct, toastr) {
     var vm = this;
     vm.model = angular.copy(SelectedProduct);
-    vm.defaultImage = vm.model.xp && vm.model.xp.Image ? vm.model.xp.Image.URL : null;
-    vm.additionalImages = vm.model.xp && vm.model.xp.Image && vm.model.xp.Image.Items && vm.model.xp.Image.Items.length ? vm.model.xp.Image.Items : null;
+    vm.defaultImage = vm.model.xp && vm.model.xp.image ? vm.model.xp.image.URL : null;
+    vm.additionalImages = vm.model.xp && vm.model.xp.image && vm.model.xp.image.Items && vm.model.xp.image.Items.length ? vm.model.xp.image.Items : null;
+
+    vm.toggleZoom = toggleZoom;
 
     vm.fileUploadOptions = {
-        keyname: 'image',
+        keyname: 'additionalImages',
+        srcKeyname: 'URL',
         folder: null,
         extensions: 'jpg, png, gif, jpeg, tiff',
         invalidExtensions: null,
@@ -17,6 +20,22 @@ function ProductImagesController(OrderCloudSDK, SelectedProduct) {
         onUpdate: patchImage,
         multiple: true
     };
+
+    function toggleZoom() {
+        return OrderCloudSDK.Products.Patch(vm.model.ID, {
+            xp: {
+                imageZoom: vm.model.xp.imageZoom
+            }
+        }).then(function(data) {
+            var message;
+            if(data.xp.imageZoom) {
+                message = 'Zoom enabled on images for ' + vm.model.Name;
+            } else {
+                message = 'Zoom disabled on images for ' + vm.model.Name;
+            }
+            toastr.success(message, 'Success');
+        });
+    }
 
     function patchImage(imageXP) {
         return OrderCloudSDK.Products.Patch(vm.model.ID, {
