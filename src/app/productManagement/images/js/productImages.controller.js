a46d32bf7c8840dd6045deb37f126469c1d015d3
Alexa Rarick <ararick@four51.com>
2017-06-13 15:28:14 -0500

    F51-244 refactor: require default image before additional images are added and before image zoom is enabled

--- src/app/productManagement/images/js/productImages.controller.js
+++ src/app/productManagement/images/js/productImages.controller.js
@@ -2,13 +2,34 @@ angular.module('orderCloud')
     .controller('ProductImagesCtrl', ProductImagesController)
 ;
 
-function ProductImagesController(OrderCloudSDK, SelectedProduct, toastr) {
+function ProductImagesController(OrderCloudSDK, SelectedProduct, toastr, $state, $exceptionHandler, ocImagesModal) {
     var vm = this;
     vm.model = angular.copy(SelectedProduct);
     vm.defaultImage = vm.model.xp && vm.model.xp.image ? vm.model.xp.image.URL : null;
     vm.additionalImages = vm.model.xp && vm.model.xp.image && vm.model.xp.image.Items && vm.model.xp.image.Items.length ? vm.model.xp.image.Items : null;
 
+    if (!vm.defaultImage || !vm.additionalImages) vm.model.xp.imageZoom = false;
+
+    vm.fileUploadOptions = {
+        keyname: 'image',
+        srcKeyname: 'URL',
+        folder: null,
+        extensions: 'jpg, png, gif, jpeg, tiff',
+        invalidExtensions: null,
+        onUpdate: patchImage,
+        multiple: false,
+        addText: 'Upload an image',
+        replaceText: 'Replace',
+    };
+
+    vm.openImageModal = openImageModal;
     vm.toggleZoom = toggleZoom;
+    
+    function openImageModal(index) {
+        if(vm.model.xp.imageZoom) {
+            ocImagesModal.Open(vm.model.xp.image, index)
+        }
+    }
 
     function toggleZoom() {
         return OrderCloudSDK.Products.Patch(vm.model.ID, {
@@ -24,5 +45,18 @@ function ProductImagesController(OrderCloudSDK, SelectedProduct, toastr) {
             }
             toastr.success(message, 'Success');
         });
+    }
+
+    function patchImage(imageXP) {
+        return OrderCloudSDK.Products.Patch(vm.model.ID, {
+            xp: imageXP
+        })
+        .then(function() {
+            toastr.success('Images successfully updated', 'Success');
+            $state.go('product.images', {productid: vm.model.ID}, {reload: true});
+        })
+        .catch(function(ex) {
+            $exceptionHandler(ex);
+        })
     }
 }
